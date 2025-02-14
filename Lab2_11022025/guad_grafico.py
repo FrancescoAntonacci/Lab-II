cf2033758954e54fa2b5ed29ebe80d2561a3e59f
Francesco Antonacci <146363377+FrancescoAntonacci@users.noreply.github.com>
2025-02-14 09:16:46 +0100

    Add files via upload
    Last experience was a nightmare for me...

--- Lab2_11022025/guad_grafico.py
+++ Lab2_11022025/guad_grafico.py
@@ -1,13 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
+##Data
+filename = "/media/candido/Extreme SSD/Unipi/Secondo anno/Lab 2/Lab2_11022025/data8"
+f,sf,vin,svin, vout,svout = np.loadtxt(filename, unpack=True)
+Re=328
+sRe=5
+Rc=991
+sRc=9
+Rb=560
+sRb=5
+Ce=4.7e-6
+sCe=Ce*0.2
+etaVt=30e-3
+##Calculating gain
+G = vout/vin
+sG = [(svin/vin) + (svout/vout)]*G
+##
+def Gatt(f,bf,rb):
+    #(Rc/(Rb+etaVt/I))*(Re/(1+(Re*2*np.pi*f*Ce)**2))
+    return (bf+1)*Re/(Rb+rb+)
+##p0
+p0=(22e-6)
+##Best fit
 
-f = np.array([0.804, 1.627, 2.4464, 6.35, 15.118, 31.026, 46.493])
-sigma_f = no.array([0.1, 0.0003, 0.0001, 0.01, 0.001, 0.001, 0.001])
-v_in = np.array([20.0, 16.8, 16.0,16.8, 14.0, 11.40, 9.60])
-v_out = np.array([0.46, 0.63, 0.68, 0.62, 0.60, 0.568, 0.484])
-sigma_vin = np.array([0.6, 0.5, 0.5, 0.5, 0.4, 0.3, 0.2])
-sigma_vout = np.array([0.01, 0.03, 0.05, 0.03, 0.02, 0.02, 0.02])
 
-guad = v_out/v_in
-sigma_guad = [(sigma_vin/v_in) + (sigma_vout/v_out)]*guad
-pl
+##plot
+xx=np.linspace(min(f),max(f),1000)
+plt.figure(1)
+plt.errorbar(f, G,sG,sf,fmt='o',label='G(f)')
+plt.plot(f,Gatt(f,p0),label='Modello teorico')
+plt.xlabel('f [Hz]')
+plt.ylabel('G(f)')
+plt.legend()
+plt.grid()
+plt.show()
