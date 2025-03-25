bfe59d9bc02cb06fcc4392d6fd39297857ae8e14
Francesco Antonacci <146363377+FrancescoAntonacci@users.noreply.github.com>
2025-03-25 19:45:37 +0100

    Almost definitive version
    - adds in Glicemia
    - corrections in Pinnacolone
    - correction in Main

--- FFT/Codicini/FFT13/Simulazioni/pinnacolone.py
+++ FFT/Codicini/FFT13/Simulazioni/pinnacolone.py
@@ -55,18 +55,26 @@ def T_AC(w,wosc):
     """
     return 1/(1-1j*wosc/w)
 
-def vc(t, w, tau, a, dt):
-    return a  *T_AC(w,wosc)* np.exp(-t/tau+1j * (w * (t+dt) ))
 
 def vc(t, w, tau, a, dt):
+    return a  * np.exp(-t/tau+1j * (w * (t+dt) ))
+
+
+def vc_AC(t, w, tau, a, dt):
     return a  *T_AC(w,wosc)* np.exp(-t/tau+1j * (w * (t+dt) ))
+
 
 def vr2(t, w, tau,tau0, a, dt,theta):
     return a*(tau0/tau)*np.sqrt(1+(tau*w)**2)  * np.exp(-t/tau+1j * (w * (t+dt)+theta))
 
-def vwc_AC(t, w, a, dt,c, iter=1000):
+
+def vr2_AC(t, w, tau,tau0, a, dt,theta):
+    return a*T_AC(w,wosc)*(tau0/tau)*np.sqrt(1+(tau*w)**2)  * np.exp(-t/tau+1j * (w * (t+dt)+theta))
+
+
+def vwc_AC(t, w, a, dt,c, iter=10000):
     """
-    Real amplitude of the signal exiting the capacitator (in AC mode??)
+    Real amplitude of the signal exiting the capacitator in AC mode
 
     Params:
     -------
@@ -99,9 +107,9 @@ def vwc_AC(t, w, a, dt,c, iter=1000):
     return np.real(a *f+ c)
 
 
-def vwc(t, w, a, dt,c, iter=1000):
+def vwc(t, w, a, dt,c, iter=10000):
     """
-    Real amplitude of the signal exiting the capacitator (in AC mode??)
+    Real amplitude of the signal exiting the capacitator
 
     Params:
     -------
@@ -133,9 +141,10 @@ def vwc(t, w, a, dt,c, iter=1000):
         f += (1-rG/z)*(2 / (k * np.pi)) * np.exp(1j*(k * t * w-np.pi/2))
     return np.real(a *f+ c)
 
-def vwr_AC(t, w, a, dt,c, iter=1000):
+
+def vwr_AC(t, w, a, dt,c, iter=10000):
     """
-    Real amplitude of the signal exiting the inductor (in AC mode??)
+    Real amplitude of the signal exiting the inductor in AC mode
 
     Params:
     -------
@@ -163,9 +172,10 @@ def vwr_AC(t, w, a, dt,c, iter=1000):
         f += T_AC(w*k,wosc)*(1-rG/z-(zl*z1/(z1*z+z2*z)))*(2 / (k * np.pi)) * np.exp(1j*(k * t * w-np.pi/2))
     return np.real(a * f + c)
 
-def vwr(t, w, a, dt,c, iter=1000):
+
+def vwr(t, w, a, dt,c, iter=10000):
     """
-    Real amplitude of the signal exiting the inductor (in AC mode??)
+    Real amplitude of the signal exiting the inductor 
 
     Params:
     -------
@@ -190,7 +200,7 @@ def vwr(t, w, a, dt,c, iter=1000):
         z2=(r1+r2+1j*(w*k)*L)
         z=(rG+1/((1/z1)+(1/z2)))
         zl=1j*(k*w)*L+r1
-        f += (1-rG/z-(zl*z1/(z1*z+z2*z)))*(2 / (k * np.pi)) * np.exp(1j*(k * t * w-np.pi/2))
+        f += (r2*z1/(z1*z+z2*z))*(2 / (k * np.pi)) * np.exp(1j*(k * t * w-np.pi/2))
     return np.real(a * f + c)
 
 
@@ -226,41 +236,40 @@ def signal(t, w, a, dt,c, iter=10000):
     return np.real(a * f + c)
 
 tt1 = np.linspace(0, toscmax, 5000)
-tt2=np.linspace(toscmax,0.3, 5000)
+tt2=np.linspace(toscmax,0.2, 5000)
 
 fig, axes = plt.subplots(2, 2, figsize=(16, 10), sharex='col', sharey='col',  gridspec_kw={'hspace': 0.5, 'wspace': 0.3})
 
-axes[0,0].plot(tt1, np.real(T_AC(w,wosc)*vc(tt1, w, tau, a, dt)), label="vc(t)")
-axes[0,0].plot(tt1,np.real(T_AC(w,wosc)*vr2(tt1, w, tau,tau0, a, dt,theta)), label="vr2(t)")
-axes[0,0].plot(tt2,signal(tt2,wgenfun,a,dt,c))
-axes[0,0].plot(tt2,signal(tt2,wgenfun,a,dt,c))
+axes[0,0].plot(tt1, np.real(vc_AC(tt1, w, tau, 0.4*a, dt)), label="vc(t)")
+axes[0,0].plot(tt1,np.real(vr2_AC(tt1, w, tau,tau0, 0.4*a, dt,theta)), label="vr2(t)")
+axes[0,0].plot(tt2,np.real(vwc_AC(tt2,wgenfun,a,dt,c))+ np.real(vc_AC(tt2, w, tau, 0.4*a, dt)))
+axes[0,0].plot(tt2,np.real(vwr_AC(tt2,wgenfun,a,dt,c))+np.real(vr2_AC(tt2, w, tau,tau0, 0.4*a, dt,theta)))
 axes[0,0].set_title("Segnali oscilloscopio in AC")
 axes[0,0].set_xlabel("t[arb.un]")
 axes[0,0].set_ylabel("V[arb.un]")
 axes[0, 0].legend()
 
-axes[0,1].plot(np.real(vc(tt1, w, tau, a, dt)),np.real(vr2(tt1, w, tau,tau0, a, dt,theta)), label="XY")
-axes[0,1].plot(vwc_AC(tt2,wgenfun,a,dt,c),vwr_AC(tt2,wgenfun,a,dt,c))
+axes[0,1].plot(np.real(vc(tt1, w, tau, 0.4*a, dt)),np.real(vr2(tt1, w, tau,tau0, 0.4*a, dt,theta)), label="XY")
+axes[0,1].plot(np.real(vwc_AC(tt2,wgenfun,a,dt,c))+ np.real(vc_AC(tt2, w, tau, 0.4*a, dt)),np.real(vwr_AC(tt2,wgenfun,a,dt,c))+ np.real(vr2_AC(tt2, w, tau,tau0, 0.4*a, dt,theta)))
 axes[0,1].set_title("Segnali XY oscilloscopio in AC")
 axes[0,1].set_xlabel("V[arb.un]")
 axes[0,1].set_ylabel("V[arb.un]")
 axes[0,1].legend()
 
 
-axes[1,0].plot(tt1, np.real(vc(tt1, w, tau, a, dt)), label="vc(t)")
-axes[1,0].plot(tt1,np.real(vr2(tt1, w, tau,tau0, a, dt,theta)), label="vr2(t)")
-axes[1,0].plot(tt2,vwc(tt2,wgenfun,a,dt,c)+ np.real(vc(tt2, w, tau, a, dt)))
-axes[1,0].plot(tt2,vwr(tt2,wgenfun,a,dt,c)+np.real(vr2(tt2, w, tau,tau0, a, dt,theta)))
+axes[1,0].plot(tt1, np.real(vc(tt1, w, tau, 0.4*a, dt)), label="vc(t)")
+axes[1,0].plot(tt1,np.real(vr2(tt1, w, tau,tau0, 0.4*a, dt,theta)), label="vr2(t)")
+axes[1,0].plot(tt2,vwc(tt2,wgenfun,a,dt,c)+ np.real(vc(tt2, w, tau, 0.4*a, dt)))
+axes[1,0].plot(tt2,vwr(tt2,wgenfun,a,dt,c)+np.real(vr2(tt2, w, tau,tau0, 0.4*a, dt,theta)))
 axes[1,0].set_title("Segnali ")
 axes[1,0].set_xlabel("t[arb.un]")
 axes[1,0].set_ylabel("V[arb.un]")
-axes[1, 0].legend()
+axes[1,0].legend()
 
-axes[1,1].plot(np.real(vc(tt1, w, tau, a, dt)),np.real(vr2(tt1, w, tau,tau0, a, dt,theta)), label="XY")
-axes[1,1].plot(vwc(tt2,wgenfun,a,dt,c)+ np.real(vc(tt2, w, tau, a, dt)),vwr(tt2,wgenfun,a,dt,c)+np.real(vr2(tt2, w, tau,tau0, a, dt,theta)))
-axes[1,1].set_title("Segnali Xy")
+axes[1,1].plot(np.real(vc(tt1, w, tau, 0.4*a, dt)),np.real(vr2(tt1, w, tau,tau0, 0.4*a, dt,theta)), label="XY")
+axes[1,1].plot(vwc(tt2,wgenfun,a,dt,c)+ np.real(vc(tt2, w, tau, 0.4*a, dt)),vwr(tt2,wgenfun,a,dt,c)+np.real(vr2(tt2, w, tau,tau0, 0.4*a, dt,theta)))
+axes[1,1].set_title("Segnali XY")
 axes[1,1].set_xlabel("t[arb.un]")
 axes[1,1].set_ylabel("V[arb.un]")
 axes[1, 1].legend()
-
 plt.show()
